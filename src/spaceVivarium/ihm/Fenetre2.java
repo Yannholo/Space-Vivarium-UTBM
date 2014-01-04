569e9c7e154a08e3a3d59e557827f29d172279a6
Julien Phelippot <julien.phelippot@gmail.com>
2014-01-04 14:31:40 +0100

    link tentative between fentere conf and simulation

--- src/spaceVivarium/ihm/Fenetre2.java
+++ src/spaceVivarium/ihm/Fenetre2.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -21,12 +22,22 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.TableModel;
 
+import spaceVivarium.core.entities.Ant;
+import spaceVivarium.core.entities.Cthuli;
+import spaceVivarium.core.entities.Entity;
+import spaceVivarium.core.entities.Human;
+import spaceVivarium.core.entities.Spider;
 import spaceVivarium.core.maps.Board;
 import spaceVivarium.core.maps.xml.XmlReader;
+import spaceVivarium.core.simulation.Simulation;
 import spaceVivarium.exception.XmlFailureException;
+import spaceVivarium.thread.IHMThread;
+import spaceVivarium.thread.SimulationThread;
+import spaceVivarium.utils.ThreadUtil;
 
 //import java.awt.event.*;
 
@@ -77,6 +88,47 @@ public class Fenetre2 extends JFrame {
                 }
             }
         });
+
+        simuler.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Simulation simulation = getSimulation();
+                simulation.init();
+
+                IHMThread ihmThread = new IHMThread(simulation);
+
+                SwingUtilities.invokeLater(ihmThread);
+
+                ThreadUtil.execute(new SimulationThread(simulation, ihmThread
+                        .getSimulationPanel()));
+            }
+        });
+    }
+
+    private static Simulation getSimulation() {
+
+        /* test xml reader */
+
+        Board testmap = null;
+        try {
+            testmap = XmlReader
+                    .xmlToBoard("src\\spaceVivarium\\core\\maps\\xml\\board.xml");
+        } catch (XmlFailureException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        // Board map = new Board(50, 50, Tile.class);
+
+        java.util.Map<Class<? extends Entity>, Integer> entityConf = new Hashtable<>();
+
+        entityConf.put(Human.class, 30);
+        entityConf.put(Spider.class, 50);
+        entityConf.put(Ant.class, 35);
+        entityConf.put(Cthuli.class, 5);
+
+        return new Simulation(testmap, entityConf);
 
     }
 
