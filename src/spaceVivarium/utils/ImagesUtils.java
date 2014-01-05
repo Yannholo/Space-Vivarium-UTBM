d088e95f9ebd44460930c4300b89abd9b76e4439
Yannholo <aymeric.malle@utbm.fr>
2014-01-05 21:47:32 +0100

    inputstream

--- src/spaceVivarium/utils/ImagesUtils.java
+++ src/spaceVivarium/utils/ImagesUtils.java
@@ -2,6 +2,7 @@ package spaceVivarium.utils;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +12,7 @@ import javax.imageio.ImageIO;
 public class ImagesUtils {
 
     private static Map<URL, BufferedImage> images = new HashMap<URL, BufferedImage>();
+    private static Map<InputStream, BufferedImage> imagesIS = new HashMap<InputStream, BufferedImage>();
 
     public static BufferedImage getImage(URL chemin) {
         BufferedImage img = images.get(chemin);
@@ -18,6 +20,20 @@ public class ImagesUtils {
             try {
                 img = ImageIO.read(chemin);
                 images.put(chemin, img);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return img;
+    }
+
+    public static BufferedImage getImage(InputStream inputStream) {
+        BufferedImage img = imagesIS.get(inputStream);
+        if (img == null) {
+            try {
+                img = ImageIO.read(inputStream);
+                imagesIS.put(inputStream, img);
             } catch (IOException e) {
                 e.printStackTrace();
             }
